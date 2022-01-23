dd347e260868c906f192b912c56f3e9158601b25
Vlad-Makarenko <Vlad-Makarenko>
2022-01-23 20:11:57 +0200

    drawing area works, drawing area is called by sign in

--- client/src/drawing_area.c
+++ client/src/drawing_area.c
@@ -18,7 +18,7 @@ void draw_brush(GtkWidget *widget, gdouble x, gdouble y, GtkWidget *draw_area)
 	//check for better effectivness
 	// gtk_widget_queue_draw(draw_area);
 
-	gtk_widget_queue_draw_area(draw_area, 0, 0, 440, 300);
+	gtk_widget_queue_draw_area(draw_area, 0, 0, 600, 600);
 	
 }
 
@@ -50,7 +50,7 @@ gboolean on_draw_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
 	width = gtk_widget_get_allocated_width(widget);
 	height = gtk_widget_get_allocated_height(widget);
 
-	cairo_set_line_width(cr, 2.0);
+	cairo_set_line_width(cr, 4.0);
 
 	if (s_point_start == NULL) {
     	return FALSE;
@@ -68,12 +68,12 @@ gboolean on_draw_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
         // cairo_set_source_rgb(cr, 0.2, 0.3, 0.2);
         
 		cairo_move_to(cr, (double)old_x, (double)old_y);
-        if(ABS((point->x - old_x)) < cairo_get_line_width(cr) * 5 && ABS((point->y - old_y)) < cairo_get_line_width(cr) * 5) {
+        // if(ABS((point->x - old_x)) < cairo_get_line_width(cr) * 5 && ABS((point->y - old_y)) < cairo_get_line_width(cr) * 5) {
 		    cairo_line_to(cr, (double)point->x, (double)point->y);
 
 			// weird line version
 	        // cairo_curve_to(cr, point->x, point->y, point->x + 3, point->y + 3, point->x + 5, point->y + 5);
-        }
+        // }
 
         cairo_stroke(cr);
 
@@ -103,32 +103,30 @@ gboolean on_draw_motion_notify_event(GtkWidget *widget, GdkEventMotion *event, t
 
 void add_draw_area(t_current_window_info *current_window_info) {
 
-	gtk_widget_hide(current_window_info->main_window);
+	GtkBuilder* drawing_window_builder = gtk_builder_new_from_file(get_path_to_glade("drawing_area.glade"));
+    
+    GtkWidget *drawing_window = GTK_WIDGET(gtk_builder_get_object(drawing_window_builder, "drawing_window"));
+    g_signal_connect(drawing_window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-	// create layout form glade
-	// add layout to current window
-    gtk_builder_add_from_file(current_window_info->builder, get_path_to_glade("drawing_area.glade"), NULL);
-
-
-    GtkLayout *drawing_area_layout = GTK_LAYOUT(gtk_builder_get_object(current_window_info->builder, "draw_layout"));
-
-    GtkLayout *current_layout = GTK_LAYOUT(gtk_builder_get_object(current_window_info->builder, "sign_in_layout")); //TODO: replace layout
-
-    // gtk_widget_destroy(current_layout);
-    // gtk_container_add(GTK_CONTAINER(current_window_info->main_window), GTK_WIDGET(drawing_area_layout));
-	// current_layout = GTK_LAYOUT(gtk_builder_get_object(current_window_info->builder, "sign_in_layout"));
-	// gtk_container_add(GTK_CONTAINER(drawing_area_layout), GTK_WIDGET(current_layout));
-    gtk_container_add(GTK_CONTAINER(current_layout), GTK_WIDGET(drawing_area_layout));
-
-
-	GtkWidget *draw_area = GTK_WIDGET(gtk_builder_get_object(current_window_info->builder, "draw"));
+    
+    t_current_window_info *drawing_window_info = NULL;
+    
+    drawing_window_info = create_current_window_info();
+    drawing_window_info->main_window = drawing_window;
+    drawing_window_info->builder = drawing_window_builder;
+    
+	GtkWidget *draw_area = GTK_WIDGET(gtk_builder_get_object(drawing_window_info->builder, "draw"));
 	
-    g_signal_connect(draw_area, "button-press-event", G_CALLBACK(on_draw_button_press_event), current_window_info);
-    g_signal_connect(draw_area, "button-release-event", G_CALLBACK(on_draw_button_release_event), current_window_info);
-    g_signal_connect(draw_area, "draw", G_CALLBACK(on_draw_draw), current_window_info);
-    g_signal_connect(draw_area, "motion-notify-event", G_CALLBACK(on_draw_motion_notify_event), current_window_info);
+    g_signal_connect(draw_area, "button-press-event", G_CALLBACK(on_draw_button_press_event), drawing_window_info);
+    g_signal_connect(draw_area, "button-release-event", G_CALLBACK(on_draw_button_release_event), drawing_window_info);
+    g_signal_connect(draw_area, "draw", G_CALLBACK(on_draw_draw), drawing_window_info);
+    g_signal_connect(draw_area, "motion-notify-event", G_CALLBACK(on_draw_motion_notify_event), drawing_window_info);
     gtk_widget_set_events(draw_area, GDK_BUTTON_MOTION_MASK | GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK);
-    gtk_widget_show_all(current_window_info->main_window);
+
+    gtk_widget_show(drawing_window);
+
+    gtk_main();
+
 }
 
 
