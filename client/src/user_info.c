c3ce6dbf991953d4ded1e354fa965fc6d68a3ec0
Vladyslav Makarenko <vmakarenko@c1r6p8.khpi.ucode-connect.study>
2022-01-29 19:49:32 +0200

    replace GtkEntery -> GtkLable in user info

--- client/src/user_info.c
+++ client/src/user_info.c
@@ -13,4 +13,17 @@ void set_user_info_on_button(GtkWidget *button) {
     gtk_menu_button_set_popover (GTK_MENU_BUTTON (button), user_info_popover);
     gtk_popover_set_position(user_info_popover, GTK_POS_RIGHT);
     // gtk_widget_show_all (user_info_popover); TODO: mojet delete ?
+
+    GtkLabel *status_info = GTK_LABEL( gtk_builder_get_object(builder, "status_info"));
+    GtkLabel *phone_info = GTK_LABEL( gtk_builder_get_object(builder, "phone_info"));
+    GtkLabel *mail_info = GTK_LABEL( gtk_builder_get_object(builder, "mail_info"));
+    GtkLabel *era_info = GTK_LABEL( gtk_builder_get_object(builder, "era_info"));
+    GtkLabel *level_info = GTK_LABEL( gtk_builder_get_object(builder, "level_info"));
+
+    gtk_label_set_text(status_info, "info form database");
+    gtk_label_set_text(phone_info, "info form database");
+    gtk_label_set_text(mail_info, "info form database");
+    gtk_label_set_text(era_info, "info form database");
+    gtk_label_set_text(level_info, "info form database");
+
 }
